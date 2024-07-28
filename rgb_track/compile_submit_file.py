c6cfa55bf2ef027e25d9fe334b8e32d2cb0ec1ce
Shan-Lee2k <son.le20112k@gmail.com>
2024-07-28 11:36:22 +0700

    Update Protocol1

--- rgb_track/compile_submit_file.py
+++ rgb_track/compile_submit_file.py
@@ -3,8 +3,8 @@ import pandas as pd
 if __name__ == '__main__':
     submit_file_df = pd.DataFrame()
     for idx in range(1, 4):
-        v_dev = pd.read_csv(f'../data/4@{idx}_dev_res.txt', names=['video_id'])
-        v_test = pd.read_csv(f'../data/4@{idx}_test_res.txt', names=['video_id'])
+        v_dev = pd.read_csv(f'../data/4_{idx}_dev_res.txt', names=['video_id'])
+        v_test = pd.read_csv(f'../data/4_{idx}_test_res.txt', names=['video_id'])
         df = pd.read_csv(f'experiment_tests/protocol_4_{idx}/rgb_track/exp1_protocol4_{idx}/TestFileLogger/output_4.csv')
         df.index = df.video_id
         v = pd.concat([v_dev, v_test])
