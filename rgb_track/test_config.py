37661628b31c47738b4ce4bfbdba7c1c39f59859
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-08-07 21:08:16 +0700

    Update test_config.py

--- rgb_track/test_config.py
+++ rgb_track/test_config.py
@@ -12,7 +12,7 @@ def get_config(protocol_name):
         'ngpu': 1,
         'dataset_configs': {
             'dataset_name': 'VideoDataset',
-            'datalist_path': '../data/dev_test_list.txt',
+            'datalist_path': '../data/dev_test_list_v1.txt',
             'data_columns': [('rgb_path', 'data')],
             'target_columns': ('label', 'target'),
             'transform_source': 'model_config',
@@ -36,7 +36,13 @@ def get_config(protocol_name):
                  'show_metrics': {
                      'name': 'acer',
                  },
-                 }],
+                },
+                {'logger_type': 'test_filelogger',
+                 'show_metrics': {
+                     'name': 'acer',
+                 },
+                },
+            ],
         }
     }
 
