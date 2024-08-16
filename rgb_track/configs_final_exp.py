7643ed4b5f71d510da61b9b72514bd0424be17a6
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-08-16 21:17:15 +0700

    Update configs_final_exp.py

--- rgb_track/configs_final_exp.py
+++ rgb_track/configs_final_exp.py
@@ -82,7 +82,8 @@ train_image_transform = tv.transforms.Compose([
             ])
         ], p=0.5),
     ], key_list=['data']),
-    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 4), (L - 8, L)), 'data', 'optical_flow'),
+    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 4), (L - 8, L)), 'data', 'optical_flow'), #v1
+    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 2), (L - 12, L)), 'data', 'optical_flow'), #v2
     #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 1), (2, 4)), 'data', 'optical_flow_start'),
     postprocess_transform
 
