ef0e6cd6a93d5685ff991cbaee011e9b28aab0ad
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-08-16 21:18:00 +0700

    Update test_config.py

--- rgb_track/test_config.py
+++ rgb_track/test_config.py
@@ -26,7 +26,7 @@ def get_config(protocol_name):
                 }
             },
             'nthreads': 8,
-            'batch_size': 64,
+            'batch_size': 512,
         },
 
         'logger_config': {
