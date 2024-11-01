475e9c643f446f37a0969c02b2b4604edb51bcac
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-11-01 09:20:16 +0700

    Update configs_final_exp.py

--- rgb_track/configs_final_exp.py
+++ rgb_track/configs_final_exp.py
@@ -188,7 +188,7 @@ def get_config(protocol_name, batch_size=32, learning_rate=0.0001, THR = 0.5, pr
             'wrapper_name': 'MultiModalWrapper',
             'input_modalities': modality_list + of_modality_list + static_modality, 
             'backbone': 'simplenet112',
-            'backbone_static':'MobileNetV2',
+            'backbone_static':'MobilenetV2',
             'nclasses': 1,
             'loss': 'BCE',
             'pretrained': pretrained,
