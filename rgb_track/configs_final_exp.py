6e3769a53028389df1281dd99d56d7af4f9986b4
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-11-09 11:27:30 +0700

    Update configs_final_exp.py

--- rgb_track/configs_final_exp.py
+++ rgb_track/configs_final_exp.py
@@ -26,9 +26,9 @@ test_seq_transform = tv.transforms.Compose([
 ])
 
 train_seq_transform = tv.transforms.Compose([
-    tv.transforms.RandomApply([
-        s_transforms.DuplicateElements(1, False, ['data'], 'target', 1, True)
-    ], p=0.5),
+    #tv.transforms.RandomApply([
+    #    s_transforms.DuplicateElements(1, False, ['data'], 'target', 1, True)
+    #], p=0.5),
     s_transforms.LinspaceTransform(L, key_list=['data'], max_start_index=0),
 ])
 
