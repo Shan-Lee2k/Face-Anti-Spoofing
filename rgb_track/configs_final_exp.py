e2236d1c344eee3f366df0772c538006e9a990c3
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-11-18 17:13:15 +0700

    Update configs_final_exp.py

--- rgb_track/configs_final_exp.py
+++ rgb_track/configs_final_exp.py
@@ -43,7 +43,7 @@ postprocess_transform = tv.transforms.Compose([
     transforms.CreateNewItem(transforms.RankPooling(C=1), 'data', 'stat_r1'),
 
     transforms.DeleteKeys(['data']),
-    #transforms.DeleteKeys(['key_frame']),
+    transforms.DeleteKeys(['key_frame']),
 
     transforms.Transform4EachKey([
         transforms.Transform4EachElement([
@@ -102,8 +102,9 @@ train_image_transform = tv.transforms.Compose([
         
         #tv.transforms.RandomApply([j_transforms.ColorJitter(0.2, 0.2, 0.2, 0.2)], p=0.5),
     ], key_list=['data']),
+    
     # Create keyframe
-    #transforms.CreateNewItem(transforms.KMeanKeyFrame(NUM_K), 'data', 'key_frame'),
+    transforms.CreateNewItem(transforms.KMeanKeyFrame(NUM_K), 'data', 'key_frame'),
     
     # Create static modality
     transforms.CreateNewItem(transforms.StaticImageTransform(L,'one'), 'data', 'random_static_image'),
@@ -134,7 +135,8 @@ train_image_transform = tv.transforms.Compose([
     
     
     
-    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 4), (L - 4, L)), 'data', 'optical_flow'),
+    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 4), (L - 4, L)), 'data', 'optical_flow'),
+    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0,4), 'key_frame', 'optical_flow'),
     #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 1), (2, 4)), 'data', 'optical_flow_start'),
 
     
@@ -149,17 +151,18 @@ test_image_transform = tv.transforms.Compose([
     
     
     #Create keyframe
-    #transforms.CreateNewItem(transforms.KMeanKeyFrame(1), 'data', 'key_frame'),
+    transforms.CreateNewItem(transforms.KMeanKeyFrame(k=2), 'data', 'key_frame'),
     
     # Create static modality
     transforms.CreateNewItem(transforms.StaticImageTransform(L,'one'), 'data', 'random_static_image'),
-    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, L-1), 'data', 'optical_flow'),
+    
+    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, L-1), 'data', 'optical_flow'),
+    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, 1), 'key_frame', 'optical_flow'),
     #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, 1), 'data', 'optical_flow_start'),
 
     
     postprocess_transform
 ])
-
 
 def get_config(protocol_name, batch_size=32, learning_rate=0.0001, THR = 0.5, nepochs=5, pretrained = None):
     config = {
