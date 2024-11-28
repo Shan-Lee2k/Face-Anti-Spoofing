7dfc59a07d2b9741ec233f99f4ef74f245e274c5
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-11-28 23:24:53 +0700

    Update configs_final_exp.py

--- rgb_track/configs_final_exp.py
+++ rgb_track/configs_final_exp.py
@@ -215,7 +215,7 @@ def get_config(protocol_name, batch_size=32, learning_rate=0.0001, THR = 0.5, ne
                 'lr_config': {
                     'lr_type': 'StepLR',
                     'lr': learning_rate,
-                    'lr_decay_period': 6,
+                    'lr_decay_period': 3,
                     'lr_decay_lvl': 0.5,
                 },
                 'weight_decay': 1e-05,
