c66125887137c600b63f59e34a9adfc883bae06d
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2024-12-01 02:57:44 +0700

    Update configs_final_exp.py

--- rgb_track/configs_final_exp.py
+++ rgb_track/configs_final_exp.py
@@ -5,167 +5,203 @@ import torchvision as tv
 from at_learner_core.utils import transforms
 from at_learner_core.configs import dict_to_namespace
 from at_learner_core.utils import transforms as transforms
-
+import PIL
 from at_learner_core.utils import joint_transforms as j_transforms
 from at_learner_core.utils import sequence_transforms as s_transforms
 
 
 pretrained_weights_dict = {
-    'ImageNet_V2_Large': "C:/Users/PC/Documents/GitHub/Face-Anti-Spoofing/at_learner_core/at_learner_core/models/architectures/mobilenet_v3_large-5c1a4163.pth",
-    'ImageNet_V1_Small':  "C:/Users/PC/Documents/GitHub/Face-Anti-Spoofing/at_learner_core/at_learner_core/models/architectures/mobilenet_v3_small-047dcff4.pth",
+    "ImageNet_V2_Large": "C:/Users/PC/Documents/GitHub/Face-Anti-Spoofing/at_learner_core/at_learner_core/models/architectures/mobilenet_v3_large-5c1a4163.pth",
+    "ImageNet_V1_Small": "C:/Users/PC/Documents/GitHub/Face-Anti-Spoofing/at_learner_core/at_learner_core/models/architectures/mobilenet_v3_small-047dcff4.pth",
 }
 NUM_K = 2
 L = 16
 image_size = 112
-modality_list = ['stat_r1000', 'stat_r1']
-of_modality_list = ['optical_flow'] #, 'optical_flow_start']
-static_modality = ['random_static_image']
+modality_list = ["stat_r1000", "stat_r1"]
+of_modality_list = ["optical_flow"]  # , 'optical_flow_start']
+static_modality = ["random_static_image"]
 
-test_seq_transform = tv.transforms.Compose([
-    s_transforms.LinspaceTransform(L, key_list=['data']),
-])
+test_seq_transform = tv.transforms.Compose(
+    [
+        s_transforms.LinspaceTransform(L, key_list=["data"]),
+    ]
+)
 
-train_seq_transform = tv.transforms.Compose([
-    tv.transforms.RandomApply([
-        s_transforms.DuplicateElements(1, False, ['data'], 'target', 1, True)
-    ], p=0.2),
-    tv.transforms.RandomApply([
-        s_transforms.DuplicateElements(1, False, ['data'], 'target', 0, False)
-    ], p=0.3),
-    s_transforms.LinspaceTransform(L, key_list=['data'], max_start_index=0),
-])
+train_seq_transform = tv.transforms.Compose(
+    [
+        tv.transforms.RandomApply(
+            [s_transforms.DuplicateElements(1, False, ["data"], "target", 1, True)],
+            p=0.3,
+        ),
+        tv.transforms.RandomApply(
+            [s_transforms.DuplicateElements(1, False, ["data"], "target", 0, False)],
+            p=0.3,
+        ),
+        s_transforms.LinspaceTransform(L, key_list=["data"], max_start_index=0),
+    ]
+)
 
-preprocess_transform = transforms.Transform4EachElement([
-    transforms.RemoveBlackBorders(),
-    transforms.SquarePad(),
-    tv.transforms.Resize(image_size),
-])
-
-postprocess_transform = tv.transforms.Compose([
-    transforms.CreateNewItem(transforms.RankPooling(C=1000), 'data', 'stat_r1000'),
-    transforms.CreateNewItem(transforms.RankPooling(C=1), 'data', 'stat_r1'),
-
-    transforms.DeleteKeys(['data']),
-    #transforms.DeleteKeys(['key_frame']),
-
-    transforms.Transform4EachKey([
-        transforms.Transform4EachElement([
-            #tv.transforms.Resize(224),
-            tv.transforms.ToTensor(),
-        ]),
-        transforms.StackTensors(squeeze=True),
-        tv.transforms.Normalize(mean=[0.5, 0.5], std=[0.5, 0.5])
-    ], key_list=of_modality_list),
-
-    transforms.Transform4EachKey([
+preprocess_transform = transforms.Transform4EachElement(
+    [
+        transforms.RemoveBlackBorders(),
+        transforms.SquarePad(),
         tv.transforms.Resize(image_size),
-        tv.transforms.ToTensor(),
-        tv.transforms.Normalize(mean=[0.5], std=[0.5])],
-        key_list=modality_list),
-    
-    # TRANSFORM FOR MULTI STATIC IMAGE KMEANS
-    
-    #transforms.Transform4EachKey([
-    #    transforms.ProcessImageList(tv.transforms.Compose([
-    #        tv.transforms.Resize(256),
-    #        tv.transforms.CenterCrop(224),
-    #        tv.transforms.ToTensor(),
-    #        tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    #    ]))
-    #], key_list=static_modality),
-    
-    
-    
-    # transforms.Transform4EachKey([
-    #     tv.transforms.Resize(image_size),
-    #     tv.transforms.ToTensor(),
-    #     tv.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]
+    ]
+)
 
-    #     #,
-    #     ,key_list=static_modality),
-    
-    
-    # TRANSFORM FOR SINGLE STATIC IMAGE
-    
-    
-     transforms.Transform4EachKey([
-         tv.transforms.Resize(256),
-         tv.transforms.CenterCrop(224),
-         tv.transforms.ToTensor(),
-         tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
-       ,key_list=static_modality),
-    
-    
-    
-])
+postprocess_transform = tv.transforms.Compose(
+    [
+        transforms.CreateNewItem(transforms.RankPooling(C=1000), "data", "stat_r1000"),
+        transforms.CreateNewItem(transforms.RankPooling(C=1), "data", "stat_r1"),
+        transforms.DeleteKeys(['data']),
+        # transforms.DeleteKeys(['key_frame']),
+        transforms.Transform4EachKey(
+            [
+                transforms.Transform4EachElement(
+                    [
+                        tv.transforms.ToTensor(),
+                        tv.transforms.Resize(image_size),
+                    ]
+                ),
+                transforms.StackTensors(squeeze=True),
+                tv.transforms.Normalize(mean=[0.5, 0.5], std=[0.5, 0.5]),
+            ],
+            key_list=of_modality_list,
+        ),
+        transforms.Transform4EachKey(
+            [
+                tv.transforms.Resize(image_size),
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(mean=[0.5], std=[0.5]),
+            ],
+            key_list=modality_list,
+        ),
+        # TRANSFORM FOR MULTI STATIC IMAGE KMEANS
+        # transforms.Transform4EachKey([
+        #    transforms.ProcessImageList(tv.transforms.Compose([
+        #        tv.transforms.Resize(256),
+        #        tv.transforms.CenterCrop(224),
+        #        tv.transforms.ToTensor(),
+        #        tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        #    ]))
+        # ], key_list=static_modality),
+        # transforms.Transform4EachKey([
+        #     tv.transforms.Resize(image_size),
+        #     tv.transforms.ToTensor(),
+        #     tv.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]
+        #     #,
+        #     ,key_list=static_modality),
+        # TRANSFORM FOR SINGLE STATIC IMAGE
+        transforms.Transform4EachKey(
+            [
+                tv.transforms.Resize(256),
+                tv.transforms.CenterCrop(224),
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ],
+            key_list=static_modality,
+        ),
+    ]
+)
 
-train_image_transform = tv.transforms.Compose([
-    transforms.Transform4EachKey([
-        preprocess_transform,
+train_image_transform = tv.transforms.Compose(
+    [
+        transforms.Transform4EachKey(
+            [
+                preprocess_transform,
+                tv.transforms.RandomApply(
+                    [j_transforms.RandomRotation((-5,5))], p= 0.5
+                ),
+                tv.transforms.RandomApply(
+                    [transforms.RandomZoomWithResize((image_size-6,image_size-2), target_size=(image_size,image_size))],
+                    p= 0.5
+                ),
+                tv.transforms.RandomApply(
+                    [j_transforms.ColorJitter(0.2, 0.2, 0.2, 0.2)], p=0.5
+                ),
+            ],
+            key_list=["data"],
+        ),
+        transforms.Transform4EachKey(
+            [
+                # tv.transforms.RandomApply([j_transforms.ColorJitter(0.2, 0.2, 0.2, 0.2)], p=0.5),
+
+                tv.transforms.RandomApply(
+                    [
+                        transforms.Transform4EachElement(
+                            [
+                                tv.transforms.RandomApply(
+                                    [
+                                        tv.transforms.RandomCrop(
+                                            image_size, padding=5, pad_if_needed=True
+                                        )
+                                        
+                                    ],
+                                    p=0.5,
+                                )
+                            ]
+                        )
+                    ],
+                    p=0.5,
+                ),
+                tv.transforms.RandomApply(
+                    [
+                        transforms.Transform4EachElement(
+                            [
+                                tv.transforms.RandomApply(
+                                    [tv.transforms.ColorJitter(0.05, 0.05, 0.05, 0.00)],
+                                    p=0.5,
+                                )
+                            ]
+                        )
+                    ],
+                    p=0.5,
+                ),
+            ],
+            key_list=["data"],
+        ),
+        # Create static modality
+        transforms.CreateNewItem(
+            transforms.StaticImageTransform(L, "one"), "data", "random_static_image"
+        ),
+
+        # Create keyframe
+        #transforms.CreateNewItem(transforms.KMeanKeyFrame(k=NUM_K), "data", "key_frame"),
         
-        tv.transforms.RandomApply([j_transforms.ColorJitter(0.2, 0.2, 0.2, 0.2)], p=0.5),
-    ], key_list=['data']),
-    
-    # Create keyframe
-    #transforms.CreateNewItem(transforms.KMeanKeyFrame(NUM_K), 'data', 'key_frame'),
-    # Create static modality
-    transforms.CreateNewItem(transforms.StaticImageTransform(L,'one'), 'data', 'random_static_image'),
-    
-    
-    transforms.Transform4EachKey([
-        #tv.transforms.RandomApply([j_transforms.ColorJitter(0.2, 0.2, 0.2, 0.2)], p=0.5),
-        tv.transforms.RandomApply([
-            transforms.Transform4EachElement([
-                tv.transforms.RandomApply([
-                    tv.transforms.RandomRotation(5)
-                ], p=0.5)
-            ])], p=0.5),
-        tv.transforms.RandomApply([
-            transforms.Transform4EachElement([
-                tv.transforms.RandomApply([
-                    tv.transforms.RandomCrop(image_size, padding=5, pad_if_needed=True)
-                ], p=0.5)
-            ])
-        ], p=0.5),
-        tv.transforms.RandomApply([
-            transforms.Transform4EachElement([
-                tv.transforms.RandomApply([
-                    tv.transforms.ColorJitter(0.05, 0.05, 0.05, 0.00)
-                ], p=0.5)
-            ])
-        ], p=0.5),
-    ], key_list=['data']),
-    
-    
-    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 4), (L - 4, L)), 'data', 'optical_flow'),
-    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0,NUM_K -1), 'key_frame', 'optical_flow'),
-    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 1), (2, 4)), 'data', 'optical_flow_start'),
+        # Create OFTICAL FLOW
+        transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 4), (L - 4, L)),"data","optical_flow"),
+        
+        
+        #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0,NUM_K -1), 'key_frame', 'optical_flow'),
+        #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform((0, 1), (2, 4)), 'data', 'optical_flow_start'),
+        postprocess_transform,
+    ]
+)
 
-    
-    postprocess_transform
-
-])
-
-test_image_transform = tv.transforms.Compose([
-    transforms.Transform4EachKey([
-        preprocess_transform,
-    ], key_list=['data']),
-    
-    
-    #Create keyframe
-    #transforms.CreateNewItem(transforms.KMeanKeyFrame(k=2), 'data', 'key_frame'),
-    
-    # Create static modality
-    transforms.CreateNewItem(transforms.StaticImageTransform(L,'one'), 'data', 'random_static_image'),
-    
-    transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, L-1), 'data', 'optical_flow'),
-    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, 1), 'key_frame', 'optical_flow'),
-    #transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, 1), 'data', 'optical_flow_start'),
-
-    
-    postprocess_transform
-])
-
+test_image_transform = tv.transforms.Compose(
+    [
+        transforms.Transform4EachKey(
+            [
+                preprocess_transform,
+            ],
+            key_list=["data"],
+        ),
+        # Create keyframe
+        #transforms.CreateNewItem(transforms.KMeanKeyFrame(k=2), "data", "key_frame"),
+        # Create static modality
+        transforms.CreateNewItem(
+            transforms.StaticImageTransform(L, "one"), "data", "random_static_image"
+        ),
+        transforms.CreateNewItem(
+            transforms.LiuOpticalFlowTransform(0, L - 1), "data", "optical_flow"
+        ),
+        # transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, 1), 'key_frame', 'optical_flow'),
+        # transforms.CreateNewItem(transforms.LiuOpticalFlowTransform(0, 1), 'data', 'optical_flow_start'),
+        postprocess_transform,
+    ]
+)
 
 def get_config(protocol_name, batch_size=32, learning_rate=0.0001, THR = 0.5, nepochs=5, pretrained = None):
     config = {
@@ -218,10 +254,10 @@ def get_config(protocol_name, batch_size=32, learning_rate=0.0001, THR = 0.5, ne
                 'lr_config': {
                     'lr_type': 'StepLR',
                     'lr': learning_rate,
-                    'lr_decay_period':3,
+                    'lr_decay_period':5,
                     'lr_decay_lvl': 0.5,
                 },
-                'weight_decay': 5e-05,
+                'weight_decay': 1e-05,
             },
         },
         'test_process_config': {
